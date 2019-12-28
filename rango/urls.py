f057d26ae753c7039fd6b97215a54caed8a03c8c
gal <tanjhu@163.com>
2019-12-28 12:57:48 +0800

    about_嵌套+超鏈接

--- rango/urls.py
+++ rango/urls.py
@@ -3,5 +3,7 @@ from rango import views
 
 urlpatterns = [
     path('',views.index,name='index'),
+    path('about/',views.about,name='about'),
+    path('about/about1/',views.about1,name='about1'),
 
 ]
--- rango/views.py
+++ rango/views.py
@@ -4,4 +4,10 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse('Rango says hey there partner!')
 
+def about(request):
+    return HttpResponse("Rango says here is the about page!<br/> <a href='/rango/about/about1'>About</a>")
+
+def about1(request):
+    return HttpResponse('Finally you came there,Hero..')
+
 # Create your views here.
