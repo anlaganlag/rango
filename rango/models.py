24ef747f940ada6f15fc1e9092be898b7611bc34
gal <tanjhu@163.com>
2019-12-29 15:44:37 +0800

    minor_rivise

--- rango/models.py
+++ rango/models.py
@@ -2,10 +2,9 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
-    view = models.CharField(max_length=128, unique=True)
     
-    #class Meta:
-        #verbose_name_plural='Categories'
+    class Meta:
+        verbose_name_plural='Categories'
 
     def __str__(self):
         return self.name
