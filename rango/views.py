35554fde4e6a8e3fa0f1beaf0860810bf41bda2c
gal <tanjhu@163.com>
2019-12-29 18:23:12 +0800

    list_all_the_objs

--- rango/views.py
+++ rango/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Category
 
 def index(request):
-    context_dict = {'boldmessage':'Crunchy,creamy,cookie,candy,cupcake!'}    
+
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories':category_list}    
+
     return render(request,'rango/index.html',context=context_dict)
 
 
@@ -15,5 +19,6 @@ def about(request):
 
 def about1(request):
     return HttpResponse('Finally you came there,Hero..I say no more about....')
+
 
 
