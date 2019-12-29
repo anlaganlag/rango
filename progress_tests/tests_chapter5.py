e2a10f69a4cb84828eac590deef5f9a95f9b64aa
gal <tanjhu@163.com>
2019-12-29 16:25:44 +0800

    admin_upgrade+gitignore

--- progress_tests/tests_chapter5.py
+++ progress_tests/tests_chapter5.py
@@ -1,3 +1,21 @@
+# 
+# Tango with Django 2 Progress Tests
+# By Leif Azzopardi and David Maxwell
+# With assistance from Enzo Roiz (https://github.com/enzoroiz)
+# 
+# Chapter 5 -- Models and Databases
+# Last updated: October 3rd, 2019
+# Revising Author: David Maxwell
+# 
+
+#
+# In order to run these tests, copy this module to your tango_with_django_project/rango/ directory.
+# Once this is complete, run $ python manage.py test rango.tests_chapter5
+# 
+# The tests will then be run, and the output displayed -- do you pass them all?
+# 
+# Once you are done with the tests, delete the module. You don't need to put it in your Git repository!
+#
 
 import os
 import warnings
--- rango/models.py
+++ rango/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=1314)
+    likes = models.IntegerField(default=520)
     
     class Meta:
         verbose_name_plural='Categories'
@@ -13,6 +15,6 @@ class Page(models.Model):
     category = models.ForeignKey(Category,on_delete=models.CASCADE)
     title = models.CharField(max_length=128)
     url = models.URLField()
-    views = models.IntegerField(default=0)
+    views = models.IntegerField(default=8)
     def __str__(self):
         return self.title
